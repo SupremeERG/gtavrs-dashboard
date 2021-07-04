e97ecc97d65e85bcb2947eab06d39910ae65c85d
SupremeERG <ethang717163@icloud.com>
2021-07-04 15:09:51 -0700

    first commt

---
+++ index.js
@@ -0,0 +1,31 @@
+const PORT = 3000;
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// set middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+
+app.get('/', (req,res)=>{
+    res.render("home")
+})
+
+app.get('/contact', (req,res)=>{
+    res.render("contact")
+})
+
+app.post('/contact', (req,res)=>{
+    console.log(req.body);
+    res.send("ok")
+})
+app.get('*', (req, res) => {
+    res.status(404).send("Page not found");
+});
+
+app.listen(PORT, () => { 
+    console.log("Listening on port", PORT) 
+});
--- js/detector.js
+++
@@ -1,9 +0,0 @@
-
-const device = detect.parse(navigator.userAgent).device;
-
-if(device != null) {
-    $('head').append('<link rel="stylesheet" href="css/mobile.css"</link>')
-}
-else {
-    $('head').append('<link rel="stylesheet" href="css/stylesheet.css"</link>')
-}
