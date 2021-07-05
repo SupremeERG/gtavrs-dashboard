ebaed9f746e29723a01cd9c82d63a3544fc9315d
SupremeERG <ethang717163@icloud.com>
2021-07-04 17:42:07 -0700

    s;

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const PORT = 80;
+const fs = require("fs");
+const key = fs.readFileSync('./cert/key.pem');
+const cert = fs.readFileSync('./cert/cert.pem');
 const express = require("express");
+const https = require("https");
 const path = require("path");
 const app = express();
+const server = https.createServer({ key, cert }, app);
 
 // set middleware
 app.use(express.urlencoded({ extended: true }));
@@ -10,15 +15,15 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render("home")
 })
 
-app.get('/contact', (req,res)=>{
+app.get('/contact', (req, res) => {
     res.render("contact")
 })
 
-app.post('/contact', (req,res)=>{
+app.post('/contact', (req, res) => {
     console.log(req.body);
     res.send("ok")
 })
@@ -26,6 +31,6 @@ app.get('*', (req, res) => {
     res.status(404).send("Page not found");
 });
 
-app.listen(PORT, () => { 
-    console.log("Listening on port", PORT) 
+server.listen(PORT, () => {
+    console.log("Listening on port", PORT)
 });
