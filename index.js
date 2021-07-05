6818b16e839f6e45d4fc616feb32134149433bd3
SupremeERG <ethang717163@icloud.com>
2021-07-04 18:22:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.post('/contact', (req, res) => {
     console.log(req.body);
     res.send("ok")
     nodemailer.createTransport({
-        host: 'mail.name.com',
+        host: 'mail.cheapgta.com',
         port: 465,
         secure: true, // true for 465, false for other ports
         auth: {
