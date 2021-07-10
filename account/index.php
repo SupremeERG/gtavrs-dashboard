6f6963e611990e8db5a3f96e25f7fc8a27a6b767
SupremeERG <ethang717163@icloud.com>
2021-07-10 07:07:27 -0700

    n n n

--- account/index.php
+++ account/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:type" content="website">
     <meta property="og:title" content="ERGs Recoveries" />
-    <meta property="og:description"
-        content="Official Grand Theft Auto V Recovery Service. Our service has the most fast, trustworthy and cheap recovery packages. Our menus don't get banned and your info is never saved." />
+    <meta property="og:description" content="Official Grand Theft Auto V Recovery Service. Our service has the most fast, trustworthy and cheap recovery packages. Our menus don't get banned and your info is never saved." />
     <meta property="og:url" content="https://cheapgta.com" />
     <meta property="og:image" content="/images/icon.png" />
     <title>GTA 5 Recovery Service</title>
@@ -17,7 +17,14 @@
     <link href="/css/specific.css" rel="stylesheet">
     <link href="/images/icon.png" rel="icon">
 </head>
+
 <body>
-<h1><?php echo "Hello World" ?></h1>
+
+    <form style="border: 3px solid black;">
+    <input placeholder="Username" name="username">  
+    <input placeholder="Password" name="pwd">    
+    <button>Submit</button>
+</form>
 </body>
+
 </html>
