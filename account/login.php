4a7ce3cb5d07b36f889c90eddb566717d0e4bc4b
SupremeERG <ethang717163@icloud.com>
2021-07-10 19:56:55 -0700

    Update login.php

--- account/login.php
+++ account/login.php
@@ -1,3 +1,43 @@
+<?php
+session_start();
+
+include("connection.php");
+include("functions.php");
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    // data was posted
+    $user_name = $_POST['user_name'];
+    $password = $_POST['password'];
+    if (!empty($user_name) && !empty($password) && !is_numeric($user_name)) {
+        //read the db;
+        $query = "SELECT * from users where user_name = '$user_name' limit 1";
+
+        $result = mysqli_query($con, $query);
+
+        if ($result && mysqli_num_rows($result) > 0) {
+            $user_data = mysqli_fetch_assoc($result);
+            if($user_data['password'] == $password) {
+                $_SESSION['user_id'] = $user_data['user_id'];
+                header("Location: /");
+                die;
+            }
+            else {
+                echo "Username or password not found";
+            }
+        }
+        else {
+            echo "Username or password not found";
+        }
+
+        header("Location: login.php");
+    } else {
+        echo "Please enter some valid information";
+    }
+};
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,23 +55,77 @@
     <link href="/css/custom.min.css" rel="stylesheet"> <!-- Bootstrap 5 custom sass file (compiled)-->
     <link href="/css/stylesheet.css" rel="stylesheet">
     <link href="/css/specific.css" rel="stylesheet">
+    <link href="/css/form-ctrl.css" rel="stylesheet">
     <link href="/images/icon.png" rel="icon">
 </head>
 
 <body>
-
-    <form method="POST" style="border: 3px solid black;">
-        <div>
-            <label class="form-label">Username</label>
-            <input type="text" class="form-control" name="user_name">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">
+                <img src="/images/icon.png" class="icon" alt="GTA V Recovery Service">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Shortcut
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li>
+                                <a class="dropdown-item" href="#">some recovery pkg</a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Another action</a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/store/checkout">Checkout</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contact">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
+    </nav>
 
-        <div>
-            <label class="form-label">Password</label>
-            <input type="text" class="form-control" placeholder="Password" name="password">
-        </div>
-        <input class="btn btn-primary" value="Login" type="submit">
-    </form>
+    <div class="center border border-primary rounded" style="margin-top: 6em; border-radius: 5px; width: fit-content; padding: 5px;">
+        <h5 style="text-align: center;">Signup<br><a href="/account/login.php">Already have an account?</a></h5>
+
+        <form method="POST">
+            <div>
+                <label class="form-label">Username</label>
+                <input type="text" class="form-control inp border-primary" name="user_name">
+            </div>
+
+            <div>
+                <label class="form-label">Password</label>
+                <input type="text" class="form-control inp border-primary" name="password">
+            </div>
+            <input class="btn btn-primary btn-submit" value="Create Account" type="submit">
+        </form>
+    </div>
 </body>
 
 </html>
